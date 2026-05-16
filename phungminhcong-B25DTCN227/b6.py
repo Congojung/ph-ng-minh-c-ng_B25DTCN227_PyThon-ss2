55e476c237cfec723beb1a3fc6563fbd43f1129d
Congojung <phungminhcong234@gmail.com>
2026-05-16 23:58:02 +0700

    Update b6.py

--- phungminhcong-B25DTCN227/b6.py
+++ phungminhcong-B25DTCN227/b6.py
@@ -1,7 +1,16 @@
-number_str = input("Nhập một số thập phân: ")
+a = int(input("Nhap so a: "))
+b = int(input("Nhap so b: "))
 
-number_float = float(number_str)
+print("Cac so nguyen to trong khoang la:")
 
-rounded_number = int(round(number_float))
+for number in range(a, b + 1):
+    if number > 1:
+        is_prime = True
 
-print("Số sau khi làm tròn và chuyển thành số nguyên:", rounded_number)
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print(number)
